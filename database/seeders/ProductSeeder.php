2d967103633a7e342d0fee12185a04a5ea5acb63
UniversoCaotico1997 <loria.roberto97@gmail.com>
2023-01-12 16:03:01 +0100

    Add Seeder close#11

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,14 @@ class ProductSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $product = new Product;
+            $product->name = $faker->sentence(3);
+            $product->description = $faker->text(255);
+            $product->price = $faker->randomFloat(2);
+            $product->save();
+        }
     }
 }
